04317bbf62c7cae486921e5d7d170549374c51fc
bootyfood <antonerce@gmail.com>
2026-01-30 20:59:25 -0600

    tried editing paddle, failed

--- Paddle/paddle.gd
+++ Paddle/paddle.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var left_right_speed :float #this represents the left and right speed of the paddlee
  #speed in pixels/second. The speed must be a Vector2, but we are only conncerned witht the x componentt.
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var speed = Vector2(left_right_speed,0)
 	
 	velocity = Vector2.ZERO
@@ -11,4 +11,4 @@ func _physics_process(_delta: float) -> void:
 		velocity = speed
 	if Input.is_action_pressed("move_left"):
 		velocity = -1*speed
-	move_and_slide()
+	move_and_collide(velocity*delta)
